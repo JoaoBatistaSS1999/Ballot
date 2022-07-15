285b77ab189b756b7073429934e10a2ecc5a0ae9
João Batista <jbsouzasantos1999@gmail.com>
2022-07-15 19:39:39 -0300

    Contract and structure

---
+++ src/components/header/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { HederContainer } from "./styles";
+
+const Header: React.FC = () => {
+  return <HederContainer></HederContainer>;
+};
+export default Header;
---
+++ src/components/header/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const HederContainer = styled.nav`
+  height: 60px;
+  width: 100%;
+  background-color: #3a3a3a;
+`;
+
+export const Button = styled.button``;
